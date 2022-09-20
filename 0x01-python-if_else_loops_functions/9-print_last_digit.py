a0550189a1741ad8f978b4203149e417b9475d1a
Donna Govender <106972948+gDonnag@users.noreply.github.com>
2022-09-20 23:56:52 +0200

    Task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-if number < 0:
-last_digit = number % -(10)
-print(-(last_digit), end='')
-else:
-last_digit = number % 10
-print(last_digit, end='')
-return abs(last_digit)
+    if number < 0:
+        print("{:d}".format(-(number % -10)), end='')
+        return(-(number % -10))
+    else:
+        print("{:d}".format(number % 10), end='')
+        return(number % 10)

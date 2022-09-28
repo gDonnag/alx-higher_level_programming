148df763d5ee853a685e7a88a29fa8b7780a6d7f
Donna Govender <106972948+gDonnag@users.noreply.github.com>
2022-09-28 18:25:17 +0200

    Task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def weight_average(my_list=[]):
     if my_list and len(my_list):
         num = 0
@@ -7,5 +6,5 @@ def weight_average(my_list=[]):
         for tup in my_list:
             num += (tup[0] * tup[1])
             denom += (tup[1])
-            return (num/denom)
-        return 0
+        return (num/denom)
+    return 0
